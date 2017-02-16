c7857c1b205166ebd0d76d1b82765f771e275a60
Meir Taffel <artgardening@gmail.com>
2017-02-16 09:58:17 +0000

    Complete: checkpoint 18 assignment add logic to convert every 5th message to SPAM

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    
+    before_save :sanitze_title
+    
+    
+    def sanitized_title
+        unless title.blank?
+            title.include?(LIST_OF_BAD_WORDS) ? "SPAM" : title
+        end
+    end
+    
+    def sanitize_title
+        self.title = title.include?(LIST_OF_BAD_WORDS) ? "SPAM" : title
+    end
 end
+
+post = Post.new(title: "my title", body: "my body")
+post.title # "my title"
+post.sanitized_title # "my title"
