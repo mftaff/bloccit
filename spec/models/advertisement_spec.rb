505b3e688a4d4825110817ff080d2c4ec43b5254
Meir Taffel <artgardening@gmail.com>
2017-02-19 09:07:36 +0000

    Complete: checkpoint 19 assignment. add advertisement mvc

---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let(:advertisement) { Advertisement.create!(title: "New Ad Title", body: "New Ad Body", price: 4) }
+  
+  describe "attributes" do 
+      it "has title, body, and price attributes" do
+          expect(advertisement).to have_attributes(title: "New Ad Title", body: "New Ad Body", price: 4)
+      end
+  end
+end
+
