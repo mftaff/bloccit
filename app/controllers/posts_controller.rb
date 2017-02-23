c8c4612df604869170e9399fd7d6e5cf59463d77
Meir Taffel <artgardening@gmail.com>
2017-02-23 16:27:20 +0000

    Complete: checkpoint 27. connect posts to current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :require_sign_in, except: :show
+    
     def show
         @post = Post.find(params[:id])
     end
@@ -9,16 +11,14 @@ class PostsController < ApplicationController
     end
     
     def create
-        @post = Post.new
-        @post.title = params[:post][:title]
-        @post.body = params[:post][:body]
         @topic = Topic.find(params[:topic_id])
-        @post.topic = @topic
+        @post = @topic.posts.build(post_params)
+        @post.user = current_user
         
         if @post.save
             flash[:notice] = "Post was saved."
-            # redirect_to [@topic, @post]   # uncomment to redirect to new post.
-            redirect_to @topic              # uncomment to redirect to post's parent topic.
+            redirect_to [@topic, @post]   # uncomment to redirect to new post.
+            # redirect_to @topic              # uncomment to redirect to post's parent topic.
         else
             flash.now[:alert] = "There was an error saving the post. Please try again."
             render :new
@@ -31,13 +31,12 @@ class PostsController < ApplicationController
     
     def update
         @post = Post.find(params[:id])
-        @post.title = params[:post][:title]
-        @post.body = params[:post][:body]
+        @post.assign_attributes(post_params)
         
         if @post.save
             flash[:notice] = "#{@post.title} was updated."
-            # redirect_to [@post.topic, @post]  # uncomment to redirect to new post.
-            redirect_to @post.topic             # uncomment to redirect to post's parent topic.
+            redirect_to [@post.topic, @post]  # uncomment to redirect to new post.
+            # redirect_to @post.topic             # uncomment to redirect to post's parent topic.
         else
             flash.now[:alert] = "There was an error saving the post. Please try again."
             render :edit
@@ -54,5 +53,11 @@ class PostsController < ApplicationController
             flash.now[:alert] = "There was an error deleting the post."
             render :show
         end
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
