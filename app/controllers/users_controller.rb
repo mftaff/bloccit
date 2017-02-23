b0a8681f243bcb1818806abbce46994d911ce6d2
Meir Taffel <artgardening@gmail.com>
2017-02-23 11:56:36 +0000

    refactor user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,7 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new
-        @user.name = params[:user][:name]
-        @user.email = params[:user][:email]
-        @user.password = params[:user][:password]
-        @user.password_confirmation = params[:user][:password_confirmation]
+        @user = User.new(user_params)
         
         if @user.save
             flash[:notice] = "Welcome to Bloccit #{@user.name}!"
@@ -18,6 +14,12 @@ class UsersController < ApplicationController
             flash.now[:alert] = "There was an error creating your account. Please try again."
             render :new
         end
+    end
+    
+    private
+    
+    def user_params 
+       params.require(:user).permit(:name, :email, :password, :password_confirmation) 
     end
 end
 
